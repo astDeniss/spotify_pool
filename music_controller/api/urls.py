3c7281e606bf812814d7a7a46fb46af0e1bf3374
Dennis Ast <astasitsev@gmail.com>
2020-12-20 21:44:42 +0200

    Room model, serializer, list_view added

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import main
+from .views import RoomView
 
 urlpatterns = [
-    path('', main)
+    path('rooms/', RoomView.as_view())
 ]
--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,6 +1,10 @@
+from .serializers import RoomSerializer
+from .models import Room
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
 
 # Create your views here.
-def main(request):
-    return HttpResponse('<h1>Test<h1>')
+
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
